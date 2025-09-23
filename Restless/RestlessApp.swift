57e15b46bd7725f78fd90883dfa5a46b87fdacf7
Brian Wang-chen <bwang112233@gmail.com>
2025-09-23 17:00:52 -0400

    Feat: Environment Setup
    XCode environment setup. Includes the starter for all IOS mobile apps.
    
    Starter for all IOS mobile apps include a brief preview that displays "hello world" along with Testing folders for Restless and Restless UI

---
+++ Restless/RestlessApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestlessApp.swift
+//  Restless
+//
+//  Created by Brian Wang-chen on 9/23/25.
+//
+
+import SwiftUI
+
+@main
+struct RestlessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RestlessTests/RestlessTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestlessTests.swift
+//  RestlessTests
+//
+//  Created by Brian Wang-chen on 9/23/25.
+//
+
+import Testing
+@testable import Restless
+
+struct RestlessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
